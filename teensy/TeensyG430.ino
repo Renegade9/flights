937f13194176835b46679310565cdc41536db555
Chris Saulit <chris.saulit@gmail.com>
2016-04-04 22:24:49 -0700

    add rotary encoder (knob) functions

--- teensy/TeensyG430.ino
+++ teensy/TeensyG430.ino
@@ -5,10 +5,18 @@
  via the Teensy FlightSim Library.
 
  Hardware consists of:
-   a) Range IN and OUT rocker switch (two buttons effectively)
+
+   a) Two position rocker switch (two buttons effectively)
+      Functions:  [v RNG ^] (Zoom In and Zoom Out)
+
    b) Four pushbuttons on the right side of the unit
+      Functions:  Direct, Menu, Clear, Enter
+
    c) Five pushbuttons under the LCD screen.
+      Functions:  CDI, OBS, MSG, FPL (Flight Plan), PROC (Procedures)
+
    d) Dual Rotary Encoder with an integrated pushbutton.
+      Chapter Down/Up, Page Down/Up, Cursor mode
 
  The LCD screen is out of scope for this interface.  That has an HDMI input and
  there is a software solution/hack to get the G430 display on the small LCD.
@@ -30,6 +38,7 @@
  */
  
 #include <Bounce.h>
+#include <Encoder.h>
 
 // pin assignments
 const int rangeInPin = 2;
@@ -46,6 +55,11 @@ const int msgPin = 10;
 const int obsPin = 11;
 const int cdiPin = 12;
 
+// dual rotary encoder + pushbutton
+const int innerKnobPin1 = 14;
+const int innerKnobPin2 = 15;
+const int outerKnobPin1 = 16;
+const int outerKnobPin2 = 17;
 const int cursorPin = 18;
 
 // Special variables to access the transponder datarefs
@@ -65,15 +79,11 @@ FlightSimCommand toggleMsg;
 FlightSimCommand toggleFpl;
 FlightSimCommand toggleProc;
 
+FlightSimCommand chapterDn;
+FlightSimCommand chapterUp;
+FlightSimCommand pageDn;
+FlightSimCommand pageUp;
 FlightSimCommand pressCursor;
-
-/*
- * D2+ sim/GPS/g430n1_cursor
-D3+ sim/GPS/g430n1_chapter_dn
-D4+ sim/GPS/g430n1_chapter_up 
-D5+ sim/GPS/g430n1_page_dn
-D6+ sim/GPS/g430n1_page_up 
- */
 
 // Use the debounce functions for our push-buttons
 const int debounceMillis = 5;
@@ -91,11 +101,18 @@ Bounce msgButton = Bounce(msgPin, debounceMillis);
 Bounce fplButton = Bounce(fplPin, debounceMillis);
 Bounce procButton = Bounce(procPin, debounceMillis);
 
+Encoder innerKnob = Encoder(innerKnobPin1, innerKnobPin2);
+Encoder outerKnob = Encoder(outerKnobPin1, outerKnobPin2);
 Bounce cursorButton = Bounce(cursorPin, debounceMillis);
+
+long innerKnobValuePrev=0;    // for detecting rotary position change
+long outerKnobValuePrev=0;    // for detecting rotary position change
 
 
 // setup runs once
 void setup() {
+
+  // Serial.begin(9600);
 
   pinMode(rangeInPin, INPUT_PULLUP);
   pinMode(rangeOutPin, INPUT_PULLUP);
@@ -128,71 +145,150 @@ void setup() {
   toggleFpl = XPlaneRef("sim/GPS/g430n1_fpl");
   toggleProc = XPlaneRef("sim/GPS/g430n1_proc");
 
+  chapterDn = XPlaneRef("sim/GPS/g430n1_chapter_dn");
+  chapterUp = XPlaneRef("sim/GPS/g430n1_chapter_up");
+  pageDn = XPlaneRef("sim/GPS/g430n1_page_dn");
+  pageUp = XPlaneRef("sim/GPS/g430n1_page_up");
+
   pressCursor = XPlaneRef("sim/GPS/g430n1_cursor");
 
 }
+
+
 
 // loop runs repetitively, as long as power is on (duh)
 void loop() {
   FlightSim.update(); // causes X-Plane's changes to be received
 
-  // With the pushbuttons, I'm using the internal pull-up resistors, so
-  // the "open" reading will be high, and when pressed, will go low,
-  // so we check for "fallingEdge" to detect button presses.
 
-  rangeInButton.update();
-  if (rangeInButton.fallingEdge()) {
-    zoomIn = 1;
+  if (rangeInButton.update()) {
+    if (rangeInButton.fallingEdge()) {
+      zoomIn = 1;
+    } else {
+      zoomIn = 0;
+    }
   }
-
-  rangeOutButton.update();
-  if (rangeOutButton.fallingEdge()) {
-    zoomOut = 1;
-  }
-
-
-  directButton.update();
-  if (directButton.fallingEdge()) {
-    pressDirect = 1;
-  }
-  menuButton.update();
-  if (menuButton.fallingEdge()) {
-    pressMenu = 1;
-  }
-  clrButton.update();
-  if (clrButton.fallingEdge()) {
-    pressClr = 1;
-  }
-  entButton.update();
-  if (entButton.fallingEdge()) {
-    pressEnt = 1;
+  if (rangeOutButton.update()) {
+    if (rangeOutButton.fallingEdge()) {
+      zoomOut = 1;
+    } else {
+      zoomOut = 0;
+    }
   }
 
-  cdiButton.update();
-  if (cdiButton.fallingEdge()) {
-    toggleCdi = 1;
+
+  // read the rotary encoder, if it's changed, write appropriate commands
+  // we're getting two pulses per click, so only act on "even" changes
+
+  long innerKnobValue = innerKnob.read();
+  if (innerKnobValue != innerKnobValuePrev) {
+    int delta = innerKnobValue - innerKnobValuePrev;
+    if (delta != 0 && delta % 2 == 0)  {
+      if (delta > 0) {
+        pageDn = 1;
+        pageDn = 0;
+      } else if (delta < 0)  {
+        pageUp = 1;
+        pageUp = 0;
+      }
+      innerKnobValuePrev = innerKnobValue;
+      delay(100); // the real Garmin is not so responsive!
+    }
   }
-  obsButton.update();
-  if (obsButton.fallingEdge()) {
-    toggleObs = 1;
-  }
-  msgButton.update();
-  if (msgButton.fallingEdge()) {
-    toggleMsg = 1;
-  }
-  fplButton.update();
-  if (fplButton.fallingEdge()) {
-    toggleFpl = 1;
-  }
-  procButton.update();
-  if (procButton.fallingEdge()) {
-    toggleProc = 1;
+  long outerKnobValue = outerKnob.read();
+  if (outerKnobValue != outerKnobValuePrev) {
+    int delta = outerKnobValue - outerKnobValuePrev;
+    if (delta != 0 && delta % 2 == 0)  {
+      if (delta > 0) {
+        chapterDn = 1;
+        chapterUp = 0;
+      } else if (delta < 0)  {
+        chapterUp = 1;
+        chapterUp = 0;
+      }
+      outerKnobValuePrev = outerKnobValue;
+      delay(100); // the real Garmin is not so responsive!
+    }
   }
 
-  cursorButton.update();
-  if (cursorButton.fallingEdge()) {
-    pressCursor = 1;
+  // Command Buttons on the right hand side of the unit
+  if (directButton.update()) {
+    if (directButton.fallingEdge()) {
+      pressDirect = 1;
+    } else {
+      pressDirect = 0;
+    }
   }
+  if (menuButton.update()) {
+    if (menuButton.fallingEdge()) {
+      pressMenu = 1;
+    } else {
+      pressMenu = 0;
+    }
+  }
+  if (clrButton.update()) {
+    if (clrButton.fallingEdge()) {
+      pressClr = 1;
+    } else {
+      pressClr = 0;
+    }
+  }
+  if (entButton.update()) {
+    if (entButton.fallingEdge()) {
+      pressEnt = 1;
+    } else {
+      pressEnt = 0;
+    }
+  }
+
+
+  // Toggle buttons at the bottom of the screen.  No difference in coding,
+  // can ignore the distinction.
+  
+  if (cdiButton.update()) {
+    if (cdiButton.fallingEdge()) {
+      toggleCdi = 1;
+    } else {
+      toggleCdi = 0;
+    }
+  }
+  if (obsButton.update()) {
+    if (obsButton.fallingEdge()) {
+      toggleObs = 1;
+    } else {
+      toggleObs = 0;
+    }
+  }
+  if (msgButton.update()) {
+    if (msgButton.fallingEdge()) {
+      toggleMsg = 1;
+    } else {
+      toggleMsg = 0;
+    }
+  }
+  if (fplButton.update()) {
+    if (fplButton.fallingEdge()) {
+      toggleFpl = 1;
+    } else {
+      toggleFpl = 0;
+    }
+  }
+  if (procButton.update()) {
+    if (procButton.fallingEdge()) {
+      toggleProc = 1;
+    } else {
+      toggleProc = 0;
+    }
+  }
+
+  if (cursorButton.update()) {
+    if (cursorButton.fallingEdge()) {
+      pressCursor = 1;
+    } else {
+      pressCursor = 0;
+    }
+  }
+
 }
 
 
